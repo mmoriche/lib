93d48912823280e1702e82ae9a50a80927fefec6
an5210@kit.edu <an5210@fh2n1995.localdomain>
2019-01-23 14:36:37 +0100

    Added command githubkey to add key, eval ssh agent, etc...

---
+++ bashfiles/githubkey.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+function githubkey { 
+
+if [ -z "${GITHUB_PATH}" ]; then
+   echo 
+   echo ERROR: ENVIRONMENT VARIALBE GITHUB_PATH MUST BE DEFINED
+   echo 
+   exit
+fi
+
+eval $(ssh-agent)
+
+fnm=${GITHUB_PATH}/keys/github_rsa
+
+if [ -f "${fnm}" ]; then
+  ssh-add  ${fnm}
+else
+  echo
+  echo The key file ${fnm} has not been found
+  echo
+fi
+}
