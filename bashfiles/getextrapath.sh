522ffbe8ad525bb48ddcd3dca4782eb3af96ec85
MMG <mmoriche@ing.uc3m.es>
2019-03-02 23:38:58 +0100

    extrapath

--- bashfiles/getextrapath.sh
+++ bashfiles/getextrapath.sh
@@ -1,5 +1,9 @@
 
 function getextrapath {
+   if [ -z $1 ]; then
+      echo usage: 
+      echo 'extra=`getextrapath GITHUB`'
+   fi
    CONTAINER=$1 
    # get the part of path from the datapath
    EXTRAPATH=${PWD#"${CONTAINER}"}
