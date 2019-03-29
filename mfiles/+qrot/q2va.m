31c0184a8905cf95fe11a7dc6bbe88d09b80614d
MMG <mmoriche@ing.uc3m.es>
2019-03-29 09:46:22 +0100

    added quaternion to vector angle

---
+++ mfiles/+qrot/q2va.m
@@ -0,0 +1,16 @@
+function [v a] = q2va(q)
+
+
+qw=q(4);
+qx=q(1);
+qy=q(2);
+qz=q(3);
+
+v=zeros(3,1):
+angle = 2*acos(qw);
+v(1)=qx/sqrt(1-qw.*qw);
+v(2)=qy/sqrt(1-qw.*qw);
+v(3)=qz/sqrt(1-qw.*qw);
+
+return
+end
