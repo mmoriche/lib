9a950a916abeba480008a177b06020d2f58aa4a9
mmoriche <moriche@kit.edu>
2019-04-26 18:58:47 +0200

    glb

---
+++ mfiles/+qrot/README.m
@@ -0,0 +1,18 @@
+%
+% @brief Brief documentaion
+%
+% @author M.Moriche
+%
+% @details
+%
+% All the Euler angles used in this package assume the sequence ZYX, therefore
+% the array of Euler angles is (yaw,pitch,roll).
+%
+% Regarding the quaternions the order is qx,qy,qz,qw
+%
+% The rotation matrix is defined as the matrix that transform the coordinates
+% in the fixed reference frame into the body reference frame.
+%
+% Therefore is the tranpose of the matrix given by the Robotics toolbox from
+% Matlab.
+%
