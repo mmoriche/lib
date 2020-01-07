9d0f59d69fb3a6ac8240f55cb29058d19a894e8c
mmoriche <moriche@kit.edu>
2020-01-07 16:58:10 +0100

    glb

--- mfiles/Results.m
+++ mfiles/Results.m
@@ -311,7 +311,7 @@ function summ(self, varargin);
          fprintf(fid,ln0);
       else
          while ~isempty(tkns)
-            ln1=ln0;
+            ln1=ln0
             for i3=1:length(tkns)
                varnm=tkns{i3}{1};
                tknsharr=regexp(varnm,pattharr,'tokens');
@@ -338,7 +338,8 @@ function summ(self, varargin);
             ln0=ln1;
             tkns=regexp(ln0,pat,'tokens');
          end
-         fprintf(fid,ln1);
+
+         if ~isempty(ln1), fprintf(fid,ln1); end
    
       end
       firstchar=char(fread(fid2,1,'char'));
