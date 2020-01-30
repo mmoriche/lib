98850bfa89b5c8cc3f37b0ffa994d353e9bb355a
Manuel Guerrero <guerrero@ifh-taifun.ifh.uni-karlsruhe.de>
2020-01-30 11:49:29 +0100

    Merge branch 'master' of github.com:mmoriche/lib

--- bashfiles/taifun/spool.sh
+++ bashfiles/taifun/spool.sh
@@ -4,6 +4,8 @@
 #set -x
 #
 #spooldir="/var/spool/torque/spool/"
+
+function spool {
 spooldir="/var/spool/pbs/spool/"
 #host=`qstat -n $1 | grep + | head -1 | sed 's/+/ /' | awk '{print $1}'`
 host=`qstat -f $1 | grep exec_host | head -1 | sed 's/+/ /' | sed 's/\// /' | awk '{print $3}'`
@@ -13,4 +15,4 @@ echo "FILE:" $file
 ssh $host cat $spooldir$file | $2
 echo " " | awk '{printf "\n"}'
 #
-
+}
